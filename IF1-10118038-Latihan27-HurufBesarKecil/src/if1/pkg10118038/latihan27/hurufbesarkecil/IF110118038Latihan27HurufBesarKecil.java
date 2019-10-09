f7214ebbcaa8935b6e815a98008d30b8cea1470d
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-10-09 22:41:28 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan27-HurufBesarKecil/src/if1/pkg10118038/latihan27/hurufbesarkecil/IF110118038Latihan27HurufBesarKecil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan27.hurufbesarkecil;
+import java.util.Scanner;
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan Huruf Besar Kecil 
+ */
+public class IF110118038Latihan27HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = sc.nextLine();
+        
+        System.out.println("====Hasil Formatting====");
+        System.out.println("Huruf Besar : " + kalimat.toUpperCase());
+        System.out.println("Huruf Kecil : " + kalimat.toLowerCase());
+    }
+    
+}
